93f1e9a474b242073097c17e08f964eb1eb640e0
Nahum Velazquez <vgjn98x@gmail.com>
2024-02-27 09:33:32 -0600

    Funcion para sacar factorial

--- Desafios7.js
+++ Desafios7.js
@@ -17,3 +17,24 @@ function calculaImc(peso, altura) {
 
 let resultado = calculaImc(pesoInput, alturaInput);
 alert(`Su IMC es ${resultado}`);
+
+/**
+ * 2 - crea una funcion que calcule el valor del factorial de un numero pasado como parametro
+ */
+
+alert('Factorial de un numero');
+
+let numInput = parseInt(prompt("Ingrese el numero para calcular su factorial"));
+
+function factorialNumero(numero) {
+    let facto = 1;
+    for (let i = 1; i <= numero; i++) {
+        facto = facto * i;
+        console.log(i);
+        console.log(facto);
+    }
+    return facto;
+}
+
+let resFactorial = factorialNumero(numInput);
+alert(`El factorial del numero ${numInput} es ${resFactorial}`);
